04f241e2f9c23172893c5e98496800e6c9b73410
Carl Frenn <carl.frenn@gmail.com>
2025-10-26 21:59:12 +0100

    updated the build

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,5 @@
 @echo off
-C:\TP7\BIN\TPC.EXE pixels.pas -B -Q 
-
+REM FreeDOS-compatible loop over Pascal files (to make it work with dosemu2)
+for %%F in (*.pas) do call C:\TP7\BIN\TPC.EXE %%F -B -Q
+echo.
+exit
